d45706bf73a94c79c150f01e1b8bf74308866d63
jfschaefer <j.schaefer@jacobs-university.de>
2015-08-11 14:19:20 +0200

    very simple algorithm for generating edge label positions and orientations

--- src/main/java/de/jfschaefer/layeredgraphlayout/layout/Util.java
+++ src/main/java/de/jfschaefer/layeredgraphlayout/layout/Util.java
@@ -1,6 +1,10 @@
 package de.jfschaefer.layeredgraphlayout.layout;
 
 import de.jfschaefer.layeredgraphlayout.Node;
+import de.jfschaefer.layeredgraphlayout.util.Pair;
+import de.jfschaefer.layeredgraphlayout.layout.EdgeSegment;
+
+import java.util.ArrayList;
 
 /**
  * Created by jfschaefer on 7/31/15.
@@ -12,5 +16,18 @@ public class Util {
     }
     public static Point getBottomRightCorner(Node n, Point center) {
         return new Point(center.x + 0.5 * n.getWidth(), center.y + 0.5 * n.getHeight());
+    }
+
+    public static Pair<Point, Double> naiveGetLabelPosition(ArrayList<EdgeSegment> edge) {
+        final EdgeSegment segment = edge.get(0);
+        final double fraction = edge.size() == 1 ? 0.5 : 1d;
+        final Vector derivative = segment.getDerivativeAt(fraction);
+        double angleRaw = Math.atan2(derivative.y, derivative.x);
+        if (angleRaw > Math.PI * 0.5 && angleRaw < Math.PI * 1.5) {
+            angleRaw -= Math.PI;
+        }
+        final double angle = 365d / (2 * Math.PI) * angleRaw;
+        final Point position = segment.getAt(fraction);
+        return new Pair<Point, Double>(position, angle);
     }
 }
